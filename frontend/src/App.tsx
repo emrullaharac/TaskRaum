ac2a8fe3b98787c78bed355de7298e78c2d78d65
Emrullah <arac.emrullah@gmail.com>
2025-08-28 17:07:56 +0200

    Initial commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import icon from './assets/icon.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <img src={icon} className="icon" alt="TaskRaum icon" />
+          <h1>Task Raum</h1>
+      </div>
+
+    </>
+  )
+}
+
+export default App
