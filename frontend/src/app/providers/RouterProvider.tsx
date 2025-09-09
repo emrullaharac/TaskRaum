68f10948251ce2d33d08033815e76d5d87c66790
Emrullah <arac.emrullah@gmail.com>
2025-09-10 01:18:07 +0200

    add: About page added and Router updated

--- frontend/src/app/providers/RouterProvider.tsx
+++ frontend/src/app/providers/RouterProvider.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "../../pages/Home/HomePage";
+import AboutPage from "../../pages/About/AboutPage";
 import LoginPage from "../../pages/Auth/LoginPage";
 import RegisterPage from "../../pages/Auth/RegisterPage";
 import DashboardPage from "../../pages/Dashboard/DashboardPage";
@@ -10,11 +11,13 @@ export function RouterProvider() {
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<AboutPage />} />
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
                 <Route element={<RequireAuth />}>
                     <Route path="/app" element={<DashboardPage />} />
                 </Route>
+                <Route path="*" element={<HomePage />} />
             </Routes>
         </BrowserRouter>
     );
