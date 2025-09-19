42de92b00696283e717229f735508aa11b08ba6d
Emrullah <arac.emrullah@gmail.com>
2025-09-19 03:14:56 +0200

    refactor: WebConfig added for stable json Page objects

---
+++ backend/src/main/java/dev/taskraum/backend/config/WebConfig.java
@@ -0,0 +1,8 @@
+package dev.taskraum.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
+@Configuration
+class WebConfig {}
